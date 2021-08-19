2315e62f66cc9618c51798d79e2718e36381d46b
mariordgez <mariordgez@hotmail.com>
2021-08-19 04:12:17 -0700

    Add array of task objects

--- src/index.js
+++ src/index.js
@@ -8,21 +8,36 @@ import '@fortawesome/fontawesome-free/js/brands';
 const todoInput = document.querySelector('.todo-input');
 const todoAdd = document.querySelector('.todo-add');
 const todoList = document.querySelector('.todo-list');
-
+const taskArr = [];
 todoAdd.addEventListener('click', todo);
+class Task {
+  constructor(description, index) {
+    this.description = description;
+    this.index = index;
+    this.checked = false;
+  }
+}
 
 function todo(event) {
   event.preventDefault();
 
+  const newTask = new Task(todoInput.value, taskArr.length);
+  taskArr.push(newTask);
+  console.log(taskArr);
+  ui(newTask);
+}
+
+function ui(task) {
   const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
   const todoLi = document.createElement('li');
-  todoLi.innerText = todoInput.innerText.value;
+  todoLi.classList.add('todo-item');
+  todoLi.innerText = task.description;
   todoDiv.appendChild(todoLi);
-  const todoCheck = document.createElement('button');
+  const todoCheck = document.createElement('input');
   todoCheck.type = 'checkbox';
   todoDiv.appendChild(todoCheck);
   // Lodash, now imported by this script
   todoList.appendChild(todoDiv);
 }
-
-document.body.appendChild(component());
